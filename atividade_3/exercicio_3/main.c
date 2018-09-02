8f69d0b9d8680485487df71982da95f4a4f39022
arthur <apbiancofriber@gmail.com>
2018-09-02 01:04:46 -0300

    exercises all now have 10's

--- atividade_3/exercicio_3/main.c
+++ atividade_3/exercicio_3/main.c
@@ -13,28 +13,32 @@
 // v                                      v
 double* load_vector(const char* filename, int* out_size);
 
-struct info {
-    double* a;
-    double* b;
-    double result;
+struct arg_struct {
+    double *a;
+    double *b;
+    double c;
     int position;
+    int jobPerThread;
+    int *mod;
+    int sizeOfA;
 
 };
-void* sum(void* data);
+void *sum(void *arg);
+void *sumOdd(void *arg);
 int main(int argc, char* argv[]) {
     srand(time(NULL));
-    
+
     //Temos argumentos suficientes?
     if(argc < 4) {
-        printf("Uso: %s n_threads a_file b_file\n"
+        printf("Uso: %s n_threads a_file b_file [-silent]\n"
                "    n_threads    número de threads a serem usadas na computação\n"
                "    *_file       caminho de arquivo ou uma expressão com a forma gen:N,\n"
-               "                 representando um vetor aleatório de tamanho N\n", 
-               argv[0]);
+               "                 representando um vetor aleatório de tamanho N\n"
+               "    -silent      não imprime resultado na saída\n", argv[0]);
         return 1;
     }
-  
-    //Quantas threads?
+
+    //Quantas thgeads?
     int n_threads = atoi(argv[1]);
     if (!n_threads) {
         printf("Número de threads deve ser > 0\n");
@@ -53,44 +57,144 @@ int main(int argc, char* argv[]) {
         printf("Erro ao ler arquivo %s\n", argv[3]);
         return 1;
     }
-    
+    //int silent = argc > 4 && strcmp(argv[4], "-silent") == 0;
+                           //^--->  0 se argv[4] == "-silent"
+                           //|---> -1 se argv[4] <  "-silent"
+                           //+---> +1 se argv[4] >  "-silent"
+
     //Garante que entradas são compatíveis
     if (a_size != b_size) {
         printf("Vetores a e b tem tamanhos diferentes! (%d != %d)\n", a_size, b_size);
         return 1;
     }
 
-    pthread_t treds[n_threads];
-    struct info data;
+    struct arg_struct info;
+    // initializers
+    info.a=a;
+    info.b=b;
+    info.c=0;
+    info.jobPerThread=0;
+    info.mod=0;
+    info.sizeOfA=0;
 
-    data.a = a;
-    data.b = b;
 
-    for (int i = 0; i != a_size; ++i) {
-        data.position = i % a_size;
-        pthread_create(&treds[i], NULL, sum, (void*)&data); //how does this not work?
-        pthread_join(treds[i], NULL);
-    }
 
-    
-    
-    
-    
-    //Imprime resultado
-    printf("Produto escalar: %g\n", data.result);    
 
-    //Libera memória
+    //struct arg_struct *args = malloc(sizeof(struct arg_struct));
+    if (n_threads > a_size){
+      //printf("nthreads é maior\n" );
+      n_threads=a_size;
+    } else if (n_threads <= a_size){
+        int mod=a_size%n_threads;
+        int quocient = a_size/n_threads;
+        if(mod==0){
+          info.jobPerThread=quocient;
+          printf("MODEs %d\n",quocient);
+          //entao a quantidade de posicoes que cada thread ira operar sera o quociente
+          pthread_t thread_id[n_threads];
+
+          for (int i = 0; i < n_threads; ++i) {
+            //  int rec=(int)a_size;
+              info.position = i;
+              printf("===================  %d\n",info.position);
+              pthread_create(&thread_id[i], NULL, sum , (void*)&info);
+              pthread_join(thread_id[i], NULL);
+            }
+
+
+        } else {
+
+          info.jobPerThread=quocient;
+          info.sizeOfA=a_size;
+          printf("MODEs %d\n",quocient);
+          //entao a quantidade de posicoes que cada thread ira operar sera o quociente
+          pthread_t thread_id[n_threads];
+          info.mod = &mod;
+          for (int i = 0; i < n_threads; ++i) {
+            //  int rec=(int)a_size;
+              info.position = i;
+              printf("===================  %d\n",info.position);
+              pthread_create(&thread_id[i], NULL, sumOdd , (void*)&info);
+              pthread_join(thread_id[i], NULL);
+            }
+          ///ai o numero é quebrado
+          //entao a quantidade de posicoes que cada
+          /// thread ira operar sera o quociente mais 1 pra cada um no resto
+
+        }
+      }
+
+
+
+
+        //n_threads=a_size;
+
+      //printf("a size é maior\n");
+
+
+
+
+    //Imprime resultados
+    double resultado = info.c;
+        printf("Produto escalar: %g\n", resultado);
+
+    //Importante: libera memória
     free(a);
     free(b);
+    //free(c);
 
     return 0;
 }
-void* sum(void* data) {
-    double* a = (*((struct info*) data)).a;
-    double* b =  (*((struct info*) data)).b;
-    double* result = &(*((struct info*) data)).result;
-    int i = (*((struct info*) data)).position;
 
-    *result += a[i] * b[i];
-    return NULL;
+
+
+void *sum(void *arg){
+ int i = (*((struct arg_struct*)arg)).position;
+ double *a = (*((struct arg_struct*)arg)).a;
+ double *b = (*((struct arg_struct*)arg)).b;
+ double c = (*((struct arg_struct*)arg)).c;
+ int quocient = (*((struct arg_struct*)arg)).jobPerThread;
+
+
+  for (int positions = i*quocient; positions < (i*quocient)+quocient; positions++){
+      double pa=(a[positions]);
+      double pb=(b[positions]);
+      c += pa*pb;
+      printf("A: %g, B: %g\nC: %g\n I %d: quocient: %d\n",pa ,pb ,c ,positions , quocient);
+  }
+
+  (*((struct arg_struct*)arg)).c=c;
+
+  return NULL;
+
+}
+
+void *sumOdd(void *arg){
+ int i = (*((struct arg_struct*)arg)).position;
+ double *a = (*((struct arg_struct*)arg)).a;
+ double *b = (*((struct arg_struct*)arg)).b;
+ double c = (*((struct arg_struct*)arg)).c;
+ int quocient = (*((struct arg_struct*)arg)).jobPerThread;
+ int *mods = (*((struct arg_struct*)arg)).mod;
+ int sizeA = (*((struct arg_struct*)arg)).sizeOfA;
+
+
+
+  for (int positions = i*quocient; positions < (i*quocient)+quocient; positions++){
+      double pa=(a[positions]);
+      double pb=(b[positions]);
+      c += pa*pb;
+      printf("A: %g, B: %g\nC: %g\n I %d: quocient: %d\n",pa ,pb ,c ,positions , quocient);
+  }
+  if (*mods > 0){
+    c += a[sizeA-*mods]*b[sizeA-*mods];
+    (*mods)--;
+    printf("Done one more for excess!!! %d more remaining\n", *mods );
+
+  }
+
+  (*((struct arg_struct*)arg)).c=c;
+
+  return NULL;
+
 }
