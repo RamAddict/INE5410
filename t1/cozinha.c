525e8215c6b109b321b862a135d249c2e3489535
arthur <apbiancofriber@gmail.com>
2018-09-29 15:29:50 -0300

    added some comments to function worker

--- t1/cozinha.c
+++ t1/cozinha.c
@@ -9,7 +9,7 @@ sem_t sem_bocas;
 sem_t sem_frigideiras;
 sem_t sem_garcons;
 sem_t sem_balcao;
-
+  //! inicializa semáforos com o numero respectivo de bocas, frigideiras, garçons, cozinheiros e o tam_balcao
 void cozinha_init(int cozinheiros, int bocas, int frigideiras, int garcons,
                   int tam_balcao) {
     sem_init(&sem_cozinheiros, 0, cozinheiros);
@@ -19,6 +19,7 @@ void cozinha_init(int cozinheiros, int bocas, int frigideiras, int garcons,
     sem_init(&sem_balcao, 0, tam_balcao);
     printf("INICIALIZANDO COZINHA\n");
 }
+  //! destroi semáforos
 void cozinha_destroy() {
     sem_destroy(&sem_cozinheiros);
     sem_destroy(&sem_bocas);
@@ -27,7 +28,13 @@ void cozinha_destroy() {
     sem_destroy(&sem_balcao);
 }
 
+  //! Função que executará as tarefas não DE
 void* worker(void* work) {
+/** 
+  Função que executará as tarefas não DE. É criada uma thread que executa worker,
+  e recebe struct tarefa com o tipo do trabalho a ser feito e 
+  ingredientes que precisará para executar a tarefa.
+*/
         struct tarefa* job = (struct tarefa*) work;
         switch(job->type) {
             case 0:;
@@ -51,113 +58,44 @@ void* worker(void* work) {
         return NULL;
     }
 //////////////////////////////////CARNE///////////////////////////////////////////////////////////////////
-   void pedido_carne(pedido_t* pedido) {
-
-            //! tentando ocupar um cozinheiro
-            sem_wait(&sem_cozinheiros);
-
-            printf("Pedido %d (CARNE) iniciando!\n", pedido->id);
-
-            //! Pegando carne
-            carne_t* carne1 = create_carne();
-
-            //! Cortando carne 5MIN [DE]
-            cortar_carne(carne1);
-            //! Temperar carne 3MIN [DE]
-            temperar_carne(carne1);
-            //////////WAIT
-            //! Privatizando uma boca
-            sem_wait(&sem_bocas);
-            //! Privatizando uma frigideira
-            sem_wait(&sem_frigideiras);
-
-            //! Grelhando carne 3MIN [DE]
-            grelhar_carne(carne1);
-
-             //////////POST
-            //! Devolvendo uma boca
-            sem_post(&sem_bocas);
-            //! Devolvendo uma frigideira
-            sem_post(&sem_frigideiras);
-
-            //! cria prato
-            prato_t* plate = create_prato(*pedido);
-
-
-            //! emprata a carne
-            empratar_carne(carne1, plate);
-            //! tentando colocar no balcão
-            sem_wait(&sem_balcao);
-
-            //! notificando prato no balcão
-            notificar_prato_no_balcao(plate);
-            //! libera o cozinheiro que era responsável por este prato
-            sem_post(&sem_cozinheiros);
-
-            //! tenta chamar garçom
-            sem_wait(&sem_garcons);
-            //! libera espaço no balcão depois do garçom pegar um prato
-            sem_post(&sem_balcao);
-            //! entrega pedido
-            entregar_pedido(plate);
-            //! libera garçom que estava entregando o prato
-            sem_post(&sem_garcons);
-
-            free(pedido);
-        }  // end pedido_carnes
-
-//////////////////////////////////SPAGET///////////////////////////////////////////////////////////////////
-   void pedido_spaghetti(pedido_t* pedido) {
+void pedido_carne(pedido_t* pedido) {
 
         //! tentando ocupar um cozinheiro
         sem_wait(&sem_cozinheiros);
 
-        printf("Pedido %d (SPAGHETTI) iniciando!\n", pedido->id);
+        printf("Pedido %d (CARNE) iniciando!\n", pedido->id);
 
-        //! criando treads p fazer coisas nao DE
-        pthread_t treds[2];
+        //! Pegando carne
+        carne_t* carne1 = create_carne();
 
-        molho_t* molho = create_molho();
-        struct tarefa tarefa_molho;
-        tarefa_molho.type = 0;
-        tarefa_molho.ingrediente1 = (void*) molho;
-
+        //! Cortando carne 5MIN [DE]
+        cortar_carne(carne1);
+        //! Temperar carne 3MIN [DE]
+        temperar_carne(carne1);
+        //////////WAIT
+        //! Privatizando uma boca
         sem_wait(&sem_bocas);
-        pthread_create(&treds[0], NULL, worker, (void*)&tarefa_molho);
-
-        agua_t* agua = create_agua();
-        struct tarefa tarefa_agua;
-        tarefa_agua.type = 1;
-        tarefa_agua.ingrediente1 = (void*)agua;
-        sem_wait(&sem_bocas);
-        pthread_create(&treds[1], NULL, worker, (void*)&tarefa_agua);
-
-        bacon_t* bacon = create_bacon();
-        sem_wait(&sem_bocas);
+        //! Privatizando uma frigideira
         sem_wait(&sem_frigideiras);
-        dourar_bacon(bacon);
+
+        //! Grelhando carne 3MIN [DE]
+        grelhar_carne(carne1);
+
+            //////////POST
+        //! Devolvendo uma boca
         sem_post(&sem_bocas);
+        //! Devolvendo uma frigideira
         sem_post(&sem_frigideiras);
-        spaghetti_t* moms = create_spaghetti();
-        sem_wait(&sem_bocas);
 
-        pthread_join(treds[1], NULL);
-        
-        cozinhar_spaghetti(moms, agua);
-        sem_post(&sem_bocas);
-
+        //! cria prato
         prato_t* plate = create_prato(*pedido);
 
-        //! empratar spaghetti
 
-        pthread_join(treds[0], NULL);
-
-        empratar_spaghetti(moms, molho, bacon, plate);
-        //! libera agua depois de empratar
-        destroy_agua(agua);
-        //! espera ter espaço no balcão
+        //! emprata a carne
+        empratar_carne(carne1, plate);
+        //! tentando colocar no balcão
         sem_wait(&sem_balcao);
-        //printf("prato pronto %d, cozinheiro liberado\n", pedido.id);
+
         //! notificando prato no balcão
         notificar_prato_no_balcao(plate);
         //! libera o cozinheiro que era responsável por este prato
@@ -171,68 +109,137 @@ void* worker(void* work) {
         entregar_pedido(plate);
         //! libera garçom que estava entregando o prato
         sem_post(&sem_garcons);
+
         free(pedido);
+    }  // end pedido_carnes
 
-    }  // end pedido_spaghetti
+//////////////////////////////////SPAGET///////////////////////////////////////////////////////////////////
+void pedido_spaghetti(pedido_t* pedido) {
 
-   void pedido_sopa(pedido_t* pedido) {
+    //! tentando ocupar um cozinheiro
+    sem_wait(&sem_cozinheiros);
 
-        sem_wait(&sem_cozinheiros);
-        printf("Pedido %d (SOPA) iniciando!\n", pedido->id);
+    printf("Pedido %d (SPAGHETTI) iniciando!\n", pedido->id);
 
-        pthread_t treds[1];
+    //! criando treads p fazer coisas nao DE
+    pthread_t treds[2];
 
-        agua_t* agua = create_agua();
+    molho_t* molho = create_molho();
+    struct tarefa tarefa_molho;
+    tarefa_molho.type = 0;
+    tarefa_molho.ingrediente1 = (void*) molho;
 
-        struct tarefa job;
+    sem_wait(&sem_bocas);
+    pthread_create(&treds[0], NULL, worker, (void*)&tarefa_molho);
 
-        //! ferver_agua
-        job.type = 1;
-        job.ingrediente1 = (void*) agua;
+    agua_t* agua = create_agua();
+    struct tarefa tarefa_agua;
+    tarefa_agua.type = 1;
+    tarefa_agua.ingrediente1 = (void*)agua;
+    sem_wait(&sem_bocas);
+    pthread_create(&treds[1], NULL, worker, (void*)&tarefa_agua);
 
-        sem_wait(&sem_bocas);
-        pthread_create(&treds[0], NULL, worker, (void*)&job);
+    bacon_t* bacon = create_bacon();
+    sem_wait(&sem_bocas);
+    sem_wait(&sem_frigideiras);
+    dourar_bacon(bacon);
+    sem_post(&sem_bocas);
+    sem_post(&sem_frigideiras);
+    spaghetti_t* moms = create_spaghetti();
+    sem_wait(&sem_bocas);
 
-        legumes_t* legumes = create_legumes();
+    pthread_join(treds[1], NULL);
+    
+    cozinhar_spaghetti(moms, agua);
+    sem_post(&sem_bocas);
 
-        cortar_legumes(legumes);
+    prato_t* plate = create_prato(*pedido);
 
-        sem_wait(&sem_bocas);// consirando que fazer o caldo precisa de uma boca
+    //! empratar spaghetti
 
-        pthread_join(treds[0], NULL);
+    pthread_join(treds[0], NULL);
 
-        caldo_t* caldo =  preparar_caldo(agua);
-        sem_post(&sem_bocas);
+    empratar_spaghetti(moms, molho, bacon, plate);
+    //! libera agua depois de empratar
+    destroy_agua(agua);
+    //! espera ter espaço no balcão
+    sem_wait(&sem_balcao);
+    //printf("prato pronto %d, cozinheiro liberado\n", pedido.id);
+    //! notificando prato no balcão
+    notificar_prato_no_balcao(plate);
+    //! libera o cozinheiro que era responsável por este prato
+    sem_post(&sem_cozinheiros);
 
-        sem_wait(&sem_bocas);
+    //! tenta chamar garçom
+    sem_wait(&sem_garcons);
+    //! libera espaço no balcão depois do garçom pegar um prato
+    sem_post(&sem_balcao);
+    //! entrega pedido
+    entregar_pedido(plate);
+    //! libera garçom que estava entregando o prato
+    sem_post(&sem_garcons);
+    free(pedido);
 
-        cozinhar_legumes(legumes, caldo);
+}  // end pedido_spaghetti
 
-        sem_post(&sem_bocas);
+void pedido_sopa(pedido_t* pedido) {
 
-        prato_t* plate = create_prato(*pedido);
+    sem_wait(&sem_cozinheiros);
+    printf("Pedido %d (SOPA) iniciando!\n", pedido->id);
 
-        empratar_sopa(legumes, caldo, plate);
-        
-        sem_wait(&sem_balcao);
+    pthread_t treds[1];
 
-        notificar_prato_no_balcao(plate);
+    agua_t* agua = create_agua();
 
-        sem_post(&sem_cozinheiros);
+    struct tarefa job;
 
-        sem_wait(&sem_garcons);
+    //! ferver_agua
+    job.type = 1;
+    job.ingrediente1 = (void*) agua;
 
-        sem_post(&sem_balcao);
+    sem_wait(&sem_bocas);
+    pthread_create(&treds[0], NULL, worker, (void*)&job);
 
-        entregar_pedido(plate);
+    legumes_t* legumes = create_legumes();
 
-        sem_post(&sem_garcons);
-        free(pedido);
+    cortar_legumes(legumes);
 
-    }
+    sem_wait(&sem_bocas);// consirando que fazer o caldo precisa de uma boca
+
+    pthread_join(treds[0], NULL);
+
+    caldo_t* caldo =  preparar_caldo(agua);
+    sem_post(&sem_bocas);
+
+    sem_wait(&sem_bocas);
+
+    cozinhar_legumes(legumes, caldo);
+
+    sem_post(&sem_bocas);
+
+    prato_t* plate = create_prato(*pedido);
+
+    empratar_sopa(legumes, caldo, plate);
+    
+    sem_wait(&sem_balcao);
+
+    notificar_prato_no_balcao(plate);
+
+    sem_post(&sem_cozinheiros);
+
+    sem_wait(&sem_garcons);
+
+    sem_post(&sem_balcao);
+
+    entregar_pedido(plate);
+
+    sem_post(&sem_garcons);
+    free(pedido);
+
+}
 
 
-    void* processar_pedido(void *arg) {
+void* processar_pedido(void *arg) {
     pedido_t* pedido = (pedido_t*)arg;
     switch(pedido->prato) {
         case PEDIDO_NULL:
@@ -244,23 +251,22 @@ void* worker(void* work) {
         printf("Pedido %d (SPAGHETTI) submetido!\n", pedido->id);
         pedido_spaghetti(pedido);
         break;
-        
+
         case PEDIDO_SOPA:
         printf("Pedido %d (SOPA) submetido!\n", pedido->id);
         pedido_sopa(pedido);
         break;
-        
+
         case PEDIDO_CARNE:
         printf("Pedido %d (CARNE) submetido!\n", pedido->id);
         pedido_carne(pedido);
         break;
-        
+
         case PEDIDO__SIZE:
         free(pedido);
         break;
     }
     return NULL;
 }
-
 
 #endif
