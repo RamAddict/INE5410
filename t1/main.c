5a00d8a0c5763c85ce4b8ce4be474a8d931e01c9
arthur <apbiancofriber@gmail.com>
2018-09-30 16:43:16 -0300

    changed if inside main, was doing redundant operations

--- t1/main.c
+++ t1/main.c
@@ -47,8 +47,8 @@ void check_missing(int value, const char* name) {
 }
 
 int main(int argc, char** argv) {
-    int bocas_total = 0, bocas = 4, frigideiras = 2, fogoes = 2,
-        cozinheiros = 6, garcons = 1, balcao = 5, c = 0;
+    int bocas_total = 0, bocas = 7, frigideiras = 4, fogoes = 2,
+        cozinheiros = 6, garcons = 3, balcao = 9, c = 0;
     while (c >= 0) {
         int long_idx;
         c = getopt_long(argc, argv, "cbfrga", cmd_opts, &long_idx);
@@ -108,7 +108,7 @@ int main(int argc, char** argv) {
         }
         else {
             p->id = pedidos +1;
-            if(pedidos == sizeof(pthread_t[size_pedido_array])/sizeof(pthread_t)) {
+            if(pedidos == size_pedido_array) {
                 size_pedido_array*=2;
                 pthread_t* pedido_array_new = (pthread_t*) realloc(pedido_array, sizeof(pthread_t[size_pedido_array]));
                 if (pedido_array_new == NULL ) {
